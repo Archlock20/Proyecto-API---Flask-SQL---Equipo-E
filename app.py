c693ed7feaea3cc54202b3fb5bd3354e018d5690
Axel <archlock20@gmail.com>
2022-09-18 16:59:07 -0500

    Add - Variables de entorno && Lista de requerimientos

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from routes.contacts import contacts
 from flask_sqlalchemy import SQLAlchemy
+from config import DATABASE_CONNECTION_URI
 
 app = Flask(__name__)
 
@@ -9,7 +10,7 @@ app = Flask(__name__)
 #P.D No pongas los parentesis!!!!!
 
 app.secret_key = "secret key"
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@localhost/contactsdb'
+app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_CONNECTION_URI
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 SQLAlchemy(app)
--- index.py
+++ index.py
@@ -1,5 +1,7 @@
 from app import app
 from utils.db import db
+import config
+
 
 with app.app_context():
     db.create_all()
